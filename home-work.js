3374a56cafcb9eb7ab13017aacdc2eeb5cb57c2e
iborovikov <ivans.borovikovs@live.com>
2021-03-06 14:33:58 +0200

    Основное дз

--- home-work.js
+++ home-work.js
@@ -1,17 +1,24 @@
 
 import gallery from "./gallery-items.js";
 
-const galleryListRef = document.querySelector('.js-gallery')
-const galleryMarkup = gallery.map(makeGaleryMarkup).join('')
+const galleryListRef = document.querySelector('.js-gallery');
+const lightBoxRef = document.querySelector('.js-lightbox');
+const galleryMarkup = gallery.map(makeGaleryMarkup).join('');
+const imgRef = document.querySelector('.lightbox__image');
 
-galleryListRef.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+galleryListRef.addEventListener('click', onPhotoClick);
+galleryListRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+lightBoxRef.addEventListener('click', onCloseBtnClick);
+window.addEventListener('keydown', onEscPress);
+
 
 function makeGaleryMarkup (image) {
   const { preview, original, description } = image;
   return `<li class="gallery__item">
   <a
     class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+    href="${original}"
   >
     <img
       class="gallery__image"
@@ -21,16 +28,34 @@ function makeGaleryMarkup (image) {
     />
   </a>
 </li>`;
+};
+  
+function onPhotoClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+  event.preventDefault();
+  lightBoxRef.classList.add('is-open');
+  imgRef.setAttribute('src', event.target.dataset.source);
+  imgRef.setAttribute('alt', event.target.getAttribute('alt'));
 }
   
+function onCloseBtnClick(event) {
+  if (event.target.nodeName === 'IMG') {
+    return;
+  };
+  lightBoxRef.classList.remove('is-open');
+  imgRef.setAttribute('src', '');
+  imgRef.setAttribute('alt', '');
+};
+function onEscPress(event) {
+  if (event.code === 'Escape') {
+      lightBoxRef.classList.remove('is-open');
+  imgRef.setAttribute('src', '');
+  imgRef.setAttribute('alt', '');
+  };
   
-  
-  
-  
-  
-  
-
-
+};
 
 
 
